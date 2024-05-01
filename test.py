6f9a22c37bb0375a1a62e6a81995273f8b455f2f
ShoHuu <83387636+BusinessBo@users.noreply.github.com>
2024-05-01 14:35:31 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,183 @@
+import time
+from time import sleep
+import sys
+
+def print_lyrics():
+    first = ["Lovely day today, perfect time to open up to you"]
+    second = ["But I know that you're having fun"]
+    third = ["wouldn't wanna mess this up for you"]
+    fourth = ["But I'm happy that you're happy"]
+    fifth = ["at least I do that much for you"]
+    sixth = ["Always glad you're with me"]
+    seventh = ["This emotion will be gone before you know"]
+    eigth = ["Time changed, we're different"]
+    ninth = ["But my mind still says redundant things"]
+    tenth = ["Can I not think?"]
+    eleventh = ["Will you love this part of me?"]
+    twelfth = ["My lover is a day I can't forget"]
+
+    thirteenth = ["Furthering my distance from you"]
+    fourteenth = ["Realistically I can't leave now"]
+    fifteenth = ["But I'm okay as long as you"]
+    sixteenth = ["Keep me from going crazy"]
+    seventeenth = ["Keep me from going crazy"]
+    
+
+    
+    delay1 = 0.3
+    delay2 = 0.1
+    delay3 = 0.3
+    delay4 = 0.3
+    delay5 = 0.6
+    delay6 = 0.3
+    delay7 = 0.10
+    delay8 = 0.6
+    delay9 = 0.5
+    delay10 = 0.5
+
+    delay11 = 0.3
+    delay12 = 5
+    delay13 = 1
+    delay14 = 1
+    delay15 = 1
+    delay16 = 3
+    delay17 = 1
+
+    for line in first:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.08)
+        time.sleep(delay1)
+        print('')
+
+    for line in second:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.06)
+        time.sleep(delay2)
+        print('')
+
+    for line in third:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.08)
+        time.sleep(delay3)
+        print('')
+    
+    for line in fourth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.06)
+        time.sleep(delay4)
+        print('')
+    
+    for line in fifth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.07)
+        time.sleep(delay5)
+        print('')
+    
+    for line in sixth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.06)
+        time.sleep(delay6)
+        print('')
+
+    for line in seventh:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.10)
+        time.sleep(delay7)
+        print('')
+
+    for line in eigth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.07)
+        time.sleep(delay8)
+        print('')
+
+    for line in ninth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.08)
+        time.sleep(delay8)
+        print('')
+
+    for line in tenth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.10)
+        time.sleep(delay10)
+        print('')
+
+    for line in eleventh:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.08)
+        time.sleep(delay11)
+        print('')
+
+    for line in twelfth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.12)
+        time.sleep(delay12)
+        print('')
+    
+    for line in thirteenth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.10)
+        time.sleep(delay13)
+        print('')
+    
+    for line in fourteenth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.10)
+        time.sleep(delay14)
+        print('')
+    
+    for line in fifteenth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.08)
+        time.sleep(delay15)
+        print('')
+
+    for line in sixteenth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.15)
+        time.sleep(delay16)
+        print('')
+
+    for line in seventeenth:
+        for c in line:
+            print(c, end='')
+            sys.stdout.flush()
+            sleep(0.13 )
+        time.sleep(delay17)
+        print('')
+
+   
+print_lyrics()
